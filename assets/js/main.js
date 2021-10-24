d03142c09819758c546602ca46deecce0f6b2ce9
VanHungNguyen <vanhungnguyenbku@gmail.com>
2021-10-24 15:15:31 +0700

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ var swiper = new Swiper('.mySwiper', {
 	slidesPerView: 1,
 	spaceBetween: 30,
 	loop: true,
-	autoplay: {
-		delay: 5000,
-		disableOnInteraction: false,
-	},
+	// autoplay: {
+	// 	delay: 5000,
+	// 	disableOnInteraction: false,
+	// },
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
